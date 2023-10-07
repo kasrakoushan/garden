6768a2162062fafd426242e716f753694357c8e1
Kasra <kasra.koushan@gmail.com>
2023-10-07 13:22:35 -0400

    Add a link to the homepage on the 404 page

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -3,8 +3,9 @@ import { QuartzComponentConstructor } from "../types"
 function NotFound() {
   return (
     <article class="popover-hint">
-      <h1>404</h1>
-      <p>Either this page is private or doesn't exist.</p>
+      <h1>Oops</h1>
+      <p>I've made some migrations on this site so some links might be broken.</p>
+      <p>Try going to the <a href="https://brain.kasra.io/">homepage</a> and searching for the page you're looking for.</p>
     </article>
   )
 }
